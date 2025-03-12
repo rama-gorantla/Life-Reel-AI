29e050c45aa39df477cd1de23da3cc368dc5cb06
rama-gorantla <104051556+rama-gorantla@users.noreply.github.com>
2025-03-12 11:33:57 +0530

    Create index.tsx

---
+++ AI/app/index.tsx
@@ -0,0 +1,14 @@
+import { Image,View, StyleSheet, Platform } from 'react-native';
+import LandingScreen from './landingPage';
+
+
+
+export default function HomeScreen() {
+  return (
+     <View>
+        <LandingScreen></LandingScreen>
+     </View>
+  );
+}
+
+
