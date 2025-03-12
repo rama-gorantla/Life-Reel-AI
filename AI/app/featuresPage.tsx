b0dd76844501ad4d4f6a5f1cec548c0a9dd3b4e3
rama-gorantla <104051556+rama-gorantla@users.noreply.github.com>
2025-03-12 11:37:45 +0530

    Create featuresPage.tsx

---
+++ AI/app/featuresPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, ScrollView, TouchableOpacity, TextInput, StyleSheet, FlatList, Image } from "react-native";
+
+const FeaturesPage = () => {
+
+  return (
+
+      <View>
+          <Text>Features Screen</Text>
+      </View>
+     
+  );
+};
+
+
+export default FeaturesPage;
