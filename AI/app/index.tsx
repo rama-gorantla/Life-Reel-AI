e96a8aec424def0af67aa57c2641881875bd5f47
rama-gorantla <104051556+rama-gorantla@users.noreply.github.com>
2025-03-13 11:32:15 +0530

    Update index.tsx

--- AI/app/index.tsx
+++ AI/app/index.tsx
@@ -1,12 +1,13 @@
 import { Image,View, StyleSheet, Platform } from 'react-native';
 import LandingScreen from './landingPage';
+import OnboardingScreen from './onBoardingScreen';
 
 
 
 export default function HomeScreen() {
   return (
      <View>
-        <LandingScreen></LandingScreen>
+        <OnboardingScreen></OnboardingScreen>
      </View>
   );
 }
